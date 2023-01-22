a364f13da0f4ecd6e61ef859ba77ea867e70d59d
wombus <peter.napoli@gmail.com>
2023-01-21 22:30:58 -0500

    actually finished-ish

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -2,15 +2,11 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
-    
-    @State private var selectedPlace: Location?
-    
-    @State private var locations = [Location]()
+    @StateObject private var viewModel = ViewModel()
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+            Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
                 MapAnnotation(coordinate: location.coordinate) {
                     VStack {
                         Image(systemName: "star.circle")
@@ -24,7 +20,7 @@ struct ContentView: View {
                             .fixedSize()
                     }
                     .onTapGesture {
-                        selectedPlace = location
+                        viewModel.selectedPlace = location
                     }
                 }
             }
@@ -38,8 +34,7 @@ struct ContentView: View {
                 HStack {
                     Spacer()
                     Button {
-                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
-                        locations.append(newLocation)
+                        viewModel.addLocation()
                     } label: {
                         Image(systemName: "plus")
                     }
@@ -52,16 +47,15 @@ struct ContentView: View {
                 }
             }
         }
-        .sheet(item: $selectedPlace) {place in
-            EditView(location: place) { newLocation in
-                if let index = locations.firstIndex(of: place) {
-                    locations[index] = newLocation
+        .sheet(item: $viewModel.selectedPlace) {place in
+            EditView(location: place) {
+                viewModel.update(location: $0)
                 }
-            }
             Text(place.name)
+            }
+           
         }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ BucketList/FileManager-DocumentsDirectory.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+extension FileManager {
+    static var documnentsDirectory: URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
